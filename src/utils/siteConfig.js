5e3c7f3122895d54b310d444e168aa36be0f9885
marcoSven <me@marcosven.com>
2020-10-26 07:28:41 -0700

    changed subdomain

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://infallible-brattain-4d3d6c.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://dazzling-bassi-c3e77e.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
